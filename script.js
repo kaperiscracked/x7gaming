9afa8f2e3a3c762744233968d37ec0fd58bc83af
kaperiscracked <1081221@lwsd.org>
2024-11-25 20:30:36 -0800

    script.js Update 1.2

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function checkPassword() {
   // Define the correct password
-  const correctPassword = "q9&)k@kd17";
+  const correctPassword = "q9&)k@kd17"; 
 
-  // Get the entered password
   const enteredPassword = document.getElementById("password").value;
 
-  // Check if the entered password matches the correct password
   if (enteredPassword === correctPassword) {
     // Hide the welcome text and access instructions
     document.getElementById("welcome-text").style.display = "none";
     document.getElementById("access-text").style.display = "none";
 
-    // Hide the login section (input field and submit button)
     document.getElementById("login-section").style.display = "none";
 
-    // Show the secret content
     document.getElementById("secret-content").classList.remove("hidden");
   } else {
     alert("Incorrect password. Please try again.");
